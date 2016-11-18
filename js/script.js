d3cf96c0d6083d5062327d30941ce890e720d304
weazelteles85 <claudioteles85@gmail.com>
2016-11-18 16:54:35 -0500

    update all

--- js/script.js
+++ js/script.js
@@ -1,14 +1,44 @@
-function sayHello () {
-	var userImput = document.getElementById("userImput").value;
-	console.log(userImput);
-	var message = "<h2>Hello " + userImput + "!</h2>";
+// function sayHello () {
+// 	var userImput = document.getElementById("userImput").value;
+// 	console.log(userImput);
+// 	var message = "<h2>Hello " + userImput + "!</h2>";
 
-	document.getElementById("content").innerHTML = message;
+// 	document.getElementById("content").innerHTML = message;
 
+// 	if (userImput === "banana") {
+// 		var item = document.querySelector("h1").innerHTML;
+
+// 		item = "You have gained access!";
+// 		document.querySelector("h1").innerHTML = item;
+
+// 	}
+// }
+
+// document.querySelector("body").addEventListener("mousemove", function (event){
+// 	if (event.shiftKey === true) {
+// 		console.log("x: " + event.clientX);
+// 		console.log("y: " + event.clientY);
+// 	}
+// });
+
+function showLogScreen() {
+	document.getElementById("LogScreen").style.visibility = 'visible';
+}
+
+function hideLogScreen() {
+	document.getElementById("LogScreen").style.visibility = 'hidden';
+	document.getElementById("logError").style.visibility = 'hidden';
+	document.getElementById("passwordInput").value = "";
+}
+
+function attemptLogin() {
+	var userImput = document.getElementById("passwordInput").value;
 	if (userImput === "banana") {
-		var item = document.getElementsByTagName("h1").textContent;
-		item = "You have gained access!";
-		document.getElementsByTagName("h1").textContent = item;
-
+		document.getElementById("passwordInput").value = "";
+		window.location.href = "HomePage.html";
+	}
+	else {
+		document.getElementById("passwordInput").value = "";
+		document.getElementById("logError").style.visibility = 'visible';
 	}
 }
